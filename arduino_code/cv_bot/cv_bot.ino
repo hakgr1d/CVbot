1ed9db638c5f1a73bf1e8b6866775429905cb5d8
hakgr1d <91004543+hakgr1d@users.noreply.github.com>
2023-11-14 22:00:20 +0300

    use esp32
    test new controller

--- arduino_code/cv_bot/cv_bot.ino
+++ arduino_code/cv_bot/cv_bot.ino
@@ -1,11 +1,18 @@
-#include "ArduinoJson.h"
-#define IN1_PIN   5   // пины драйвера двигателей MX1508
-#define IN2_PIN   6   //
-#define IN3_PIN   9   //
-#define IN4_PIN   10  //  
-#define LED_PIN   13  
+//тест esp 32 devkit v1
 
-// json буфер для пакетов от orange pi
+#include "ArduinoJson.h"
+
+// пины PWM драйвера двигателей
+#define IN1_PIN 2
+#define IN2_PIN 4
+#define IN3_PIN 18
+#define IN4_PIN 19
+
+#define LED_PIN 13
+
+
+
+// json буфер для пакетов от  raspberry pi (RPI)
 StaticJsonDocument<200> jsondoc;
 
 // глобальные переменные скорости
@@ -13,57 +20,52 @@ int16_t speedA = 0;
 int16_t speedB = 0;
 
 
-void setup()
-{
-  Serial.begin(115200);
-  pinMode(IN1_PIN, OUTPUT);    
-  pinMode(IN2_PIN, OUTPUT);    
-  pinMode(IN3_PIN, OUTPUT);    
-  pinMode(IN4_PIN, OUTPUT);    
+#define UART_SPEED 115200
+
+void setup() {
+  Serial.begin(UART_SPEED);
+  pinMode(IN1_PIN, OUTPUT);
+  pinMode(IN2_PIN, OUTPUT);
+  pinMode(IN3_PIN, OUTPUT);
+  pinMode(IN4_PIN, OUTPUT);
   pinMode(LED_PIN, OUTPUT);
 }
 
-void loop()
-{
-  DeserializationError err = deserializeJson(jsondoc, Serial);  // получаем сообщение от orange pi через uart
-  if (err == DeserializationError::Ok)    // если cообщение принято
+void loop() {
+  DeserializationError err = deserializeJson(jsondoc, Serial);  // получаем сообщение от RPI через uart
+  if (err == DeserializationError::Ok)                          // если cообщение принято
   {
     speedA = (float)jsondoc["speedA"] * 2.55;  // приходит диапазон [-100; 100]
     speedB = (float)jsondoc["speedB"] * 2.55;  // расширяем до [-255; 255]
-  }
-  else
-  {
-    while (Serial.available() > 0) Serial.read(); // чистим буфер
+  } else {
+    while (Serial.available() > 0) Serial.read();  // чистим буфер
   }
   moveA(speedA);  // обновляем состояние скоростей моторов
-  moveB(speedB);  //
+  moveB(speedB);
   delay(10);
 }
 
-void moveA(int16_t speed){
+void moveA(int16_t speed) {
   //speed = -speed;
-  if(speed >= 0){
-    analogWrite(IN1_PIN, LOW);  
+  if (speed >= 0) {
+    analogWrite(IN1_PIN, LOW);
     analogWrite(IN2_PIN, abs(speed));
     digitalWrite(LED_BUILTIN, LOW);
-  } 
-  else {
-    analogWrite(IN1_PIN, abs(speed));  
+  } else {
+    analogWrite(IN1_PIN, abs(speed));
     analogWrite(IN2_PIN, LOW);
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
 
-void moveB(int16_t speed){
-  if(speed >= 0){
-    analogWrite(IN3_PIN, LOW);  
+void moveB(int16_t speed) {
+  if (speed >= 0) {
+    analogWrite(IN3_PIN, LOW);
     analogWrite(IN4_PIN, abs(speed));
     digitalWrite(LED_BUILTIN, LOW);
-  } 
-  else {
-    analogWrite(IN3_PIN, abs(speed));  
+  } else {
+    analogWrite(IN3_PIN, abs(speed));
     analogWrite(IN4_PIN, LOW);
     digitalWrite(LED_BUILTIN, HIGH);
   }
 }
- 
